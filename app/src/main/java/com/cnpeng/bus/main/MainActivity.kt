c625c1eba945ca0fae25a55a868de5aca9693472
CnPeng <893612134@qq.com>
2018-02-02 09:20:38 +0800

    完善线路信息

--- app/src/main/java/com/cnpeng/bus/main/MainActivity.kt
+++ app/src/main/java/com/cnpeng/bus/main/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.cnpeng.bus.main
 
 import android.annotation.SuppressLint
-import android.content.*
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
 import android.view.MenuItem
+import android.view.animation.ScaleAnimation
 import android.webkit.WebSettings
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
@@ -18,20 +22,16 @@ import com.cnpeng.bus.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import android.support.v7.app.AlertDialog
-import android.view.View
-import android.view.animation.ScaleAnimation
 
 
 /**
  *  CnPeng 180131
  *
  *  1、确定基色:亮天蓝色
- *  2、线路选择：1、右上角按钮    2、侧滑界面中条目   3、底部悬浮按钮
+ *  2、线路选择：1、右上角按钮（临时取消）    2、侧滑界面中条目   3、底部悬浮按钮
  *  3、双击返回退出APP
  *  4、底部切换线路的按钮允许用户动态拖拽定义在左侧或者右侧，点击选线路，长按拖拽
- *  5、右上角改成收藏线路功能
- *  6、不要在主线程加载网络页面
+ *  5、右上角改成收藏线路功能（暂未添加）
  *
  */
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
@@ -65,31 +65,29 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
      * 初始化右下角选择线路的按钮--点击展示线路列表
      */
     private fun initShowLineTv() {
-        val lineNums= arrayOf("1","2","3","4","5","6")
-       tv_showLines.setOnClickListener(View.OnClickListener { view ->
-           //执行缩放动画
-          val anim=ScaleAnimation(0.8f,1.0f,0.8f,1.0f,0.5f,0.5f)
-           anim.duration=600
-           tv_showLines.startAnimation(anim)
+        val lineNums = arrayOf("K1 正泰大酒店<-->华鲁制药", "K2 暂未开通", "K3 汽车站<-->三中", "K4 暂未开通", "K5 县政府<-->华鲁制药", "K6 汽车站<-->汽车站")
+        tv_showLines.setOnClickListener({
+            //执行缩放动画
+            val anim = ScaleAnimation(1.0f, 1.2f, 1.0f, 1.2f, 0.5f, 0.5f)
+            anim.duration = 500
+            tv_showLines.startAnimation(anim)
 
-
-           //展示线路列表弹窗
-           val lineListDialog=AlertDialog.Builder(this)
-           lineListDialog.setSingleChoiceItems(lineNums,0, DialogInterface.OnClickListener {
-               dialogInterface, index ->
-               //切换线路，因为索引从0 开始，所以此处+1
-               changeLineNum((index+1).toString())
-           })
-           lineListDialog.setTitle("请选择线路")
-           lineListDialog.show()
-       })
-
+            //展示线路列表弹窗
+            val lineListDialog = AlertDialog.Builder(this)
+            lineListDialog.setSingleChoiceItems(lineNums, 0, { dialogInterface, index ->
+                //切换线路，因为索引从0 开始，所以此处+1
+                changeLineNum((index + 1).toString())
+            })
+            lineListDialog.setTitle("请选择线路")
+            lineListDialog.setPositiveButton("确定", { _, _ -> lineListDialog.create().dismiss() })
+            lineListDialog.show()
+        })
     }
 
     /**
      * 初始化广播接收器
      */
-    fun initBroadCastReceiver() {
+    private fun initBroadCastReceiver() {
         val intentFilter = IntentFilter()
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE")
         netWorkChangeReceiver = NetWorkChangeReceiver()
@@ -148,51 +146,51 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        //menu.getItem(0).setShowAsAction(SHOW_AS_ACTION_WITH_TEXT)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.item_line1 -> {
-                changeLineNum("1")
-                return true
-            }
-            R.id.item_line2 -> {
-                changeLineNum("2")
-                return true
-            }
-            R.id.item_line3 -> {
-                changeLineNum("3")
-                return true
-            }
-            R.id.item_line4 -> {
-                changeLineNum("4")
-                return true
-            }
-            R.id.item_line5 -> {
-                changeLineNum("5")
-                return true
-            }
-            R.id.item_line6 -> {
-                changeLineNum("6")
-                return true
-            }
-//            R.id.item_changeLine -> {
-//                invalidateOptionsMenu()
-//                openOptionsMenu()
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        menuInflater.inflate(R.menu.menu_main, menu)
+//        //menu.getItem(0).setShowAsAction(SHOW_AS_ACTION_WITH_TEXT)
+//        return true
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        when (item.itemId) {
+//            R.id.item_line1 -> {
+//                changeLineNum("1")
 //                return true
 //            }
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-    
+//            R.id.item_line2 -> {
+//                changeLineNum("2")
+//                return true
+//            }
+//            R.id.item_line3 -> {
+//                changeLineNum("3")
+//                return true
+//            }
+//            R.id.item_line4 -> {
+//                changeLineNum("4")
+//                return true
+//            }
+//            R.id.item_line5 -> {
+//                changeLineNum("5")
+//                return true
+//            }
+//            R.id.item_line6 -> {
+//                changeLineNum("6")
+//                return true
+//            }
+////            R.id.item_changeLine -> {
+////                invalidateOptionsMenu()
+////                openOptionsMenu()
+////                return true
+////            }
+//            else -> return super.onOptionsItemSelected(item)
+//        }
+//    }
+
     /**
      * 切换线路
      * 如果要切换的与当前正在展示的不一致，才执行切换操作
@@ -258,6 +256,5 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 }
             }
         }
-
     }
 }
